9e0b9ed8d352ff3b518ee12cab5c60a32d4ddad2
Pawel Rojek <pawel.rojek94@gmail.com>
2022-01-28 10:14:38 +0100

    Initial commit

---
+++ Debug/Core/mydrivers/Src/stm32f401xe_i2c.d
@@ -0,0 +1,32 @@
+Core/mydrivers/Src/stm32f401xe_i2c.o: \
+ ../Core/mydrivers/Src/stm32f401xe_i2c.c \
+ ../Core/mydrivers/Inc/stm32f401xe_gpio.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Core/mydrivers/Inc/stm32f401xe_i2c.h \
+ ../Core/mydrivers/Inc/stm32f401xe_rcc.h
+
+../Core/mydrivers/Inc/stm32f401xe_gpio.h:
+
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+
+../Drivers/CMSIS/Include/core_cm4.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Include/mpu_armv7.h:
+
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+../Core/mydrivers/Inc/stm32f401xe_i2c.h:
+
+../Core/mydrivers/Inc/stm32f401xe_rcc.h:
---
+++ Debug/Core/mydrivers/Src/subdir.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/mydrivers/Src/stm32f401xe_gpio.c \
+../Core/mydrivers/Src/stm32f401xe_i2c.c \
+../Core/mydrivers/Src/stm32f401xe_pwr.c \
+../Core/mydrivers/Src/stm32f401xe_rcc.c \
+../Core/mydrivers/Src/stm32f401xe_tim.c 
+
+OBJS += \
+./Core/mydrivers/Src/stm32f401xe_gpio.o \
+./Core/mydrivers/Src/stm32f401xe_i2c.o \
+./Core/mydrivers/Src/stm32f401xe_pwr.o \
+./Core/mydrivers/Src/stm32f401xe_rcc.o \
+./Core/mydrivers/Src/stm32f401xe_tim.o 
+
+C_DEPS += \
+./Core/mydrivers/Src/stm32f401xe_gpio.d \
+./Core/mydrivers/Src/stm32f401xe_i2c.d \
+./Core/mydrivers/Src/stm32f401xe_pwr.d \
+./Core/mydrivers/Src/stm32f401xe_rcc.d \
+./Core/mydrivers/Src/stm32f401xe_tim.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/mydrivers/Src/%.o: ../Core/mydrivers/Src/%.c Core/mydrivers/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../Core/Inc -I../Core/mydrivers/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
